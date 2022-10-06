e6877c6ddbd1a03f633678d4583f0e52e5c8feaa
Thomas Brewitt <twbrewitt@gmail.com>
2022-10-06 07:46:38 -0400

    Added test bench SPARK arcade drive functionality

--- TestBench/src/main/java/frc/robot/RobotContainer.java
+++ TestBench/src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +19,23 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final DriveTrain m_dDriveTrain = new DriveTrain();
+  private final Joystick joystick1 = new Joystick(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    m_dDriveTrain.setDefaultCommand(
+      new RunCommand(
+        () -> m_dDriveTrain.doDrive(
+          joystick1.getRawAxis(1), 
+          joystick1.getTwist(), 
+          Math.max(0, 1-joystick1.getRawAxis(3))
+          ),
+      m_dDriveTrain)
+    );
   }
 
   /**
@@ -43,6 +53,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
---
+++ TestBench/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class DriveTrain extends SubsystemBase {
+  private CANSparkMax FL = new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax BL = new CANSparkMax(2, MotorType.kBrushless);
+  private CANSparkMax FR = new CANSparkMax(4, MotorType.kBrushless);
+  private CANSparkMax BR = new CANSparkMax(3, MotorType.kBrushless);
+  private boolean arcade = true;
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {}
+
+  public void switchMode() {
+    arcade = !arcade;
+  }
+
+  public void arcadeDrive(double throttle, double rotation, double speed) {
+    double MtrPower = (throttle + rotation) * speed;
+
+    FL.set(MtrPower);
+    BL.set(MtrPower);
+    FR.set(MtrPower);
+    BR.set(MtrPower);
+  }
+
+  public void tankDrive(double lThrottle, double rThrottle, double speed) {
+    lThrottle *= speed;
+    rThrottle *= speed;
+
+    FL.set(lThrottle);
+    BL.set(lThrottle);
+    FR.set(-rThrottle);
+    BR.set(-rThrottle);
+  }
+
+  public void doDrive(double throttle, double twist, double speed) {
+    if (arcade) {
+      arcadeDrive(throttle, twist, speed);
+    } else {
+      tankDrive(throttle, twist, speed);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
