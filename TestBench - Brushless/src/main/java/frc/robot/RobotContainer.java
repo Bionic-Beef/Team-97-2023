270dccadd60f24915342ae48e8523724b3bf794f
Thomas Brewitt <twbrewitt@gmail.com>
2023-03-03 17:52:51 -0500

    Camera readded

--- TestBench - Brushless/src/main/java/frc/robot/RobotContainer.java
+++ TestBench - Brushless/src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.cameraserver.CameraServer;
 
 
 /**
@@ -53,6 +54,8 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    CameraServer.startAutomaticCapture();
 
     m_dDriveTrain.setDefaultCommand(
 
