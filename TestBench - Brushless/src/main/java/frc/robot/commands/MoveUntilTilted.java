693e78f8e9899a2a0bffefa3bf1cc524f254d0d2
Brian Liu <86639831+brianliu8709@users.noreply.github.com>
2023-03-11 14:42:44 -0500

    move until tilted added

---
+++ TestBench - Brushless/src/main/java/frc/robot/commands/MoveUntilTilted.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+import utilities.IMUWrapper;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import java.lang.Math;
+
+/** An example command that uses an example subsystem. */
+public class MoveUntilTilted extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveTrain m_DriveTrain;
+  PIDController gyroZPID = new PIDController(Constants.GyroZKP, Constants.GyroZKI, Constants.GyroZKD);
+  //position variables are measured in encoder ticks
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public MoveUntilTilted(DriveTrain train, double togo) {
+    m_DriveTrain = train;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_DriveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize()
+  {
+    m_DriveTrain.doDrive(.3, .3);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute()
+  {
+    // m_DriveTrain.doDrive(.3, .3);
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //   m_DriveTrain.doDrive(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Math.abs(IMUWrapper.getYAngle()) > 10;
+  }
+}
